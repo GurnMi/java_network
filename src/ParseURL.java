962899ffb9a796d78161eb8fc79586c04780305a
GurnMi <dlrjsak3@naver.com>
2017-09-26 15:04:22 +0900

    openstream add line

--- src/ParseURL.java
+++ src/ParseURL.java
@@ -1,24 +1,33 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ParseURL {
 
-	public static void main(String[] args) {
-		URL opiniion = null;
+	public static void main(String[] args) throws IOException {
+		URL opinion = null;
 		URL homePage = null;
 		
 		try {
 			homePage = new URL("http://www.dgit.or.kr");
-			opiniion = new URL(homePage, "SUB/?ctId=15");
-			System.out.printf("protocal : %s", opiniion.getProtocol());
-			System.out.printf("host : %s", opiniion.getHost());
-			System.out.printf("port : %s", opiniion.getPort());
-			System.out.printf("path : %s", opiniion.getPath());
-			System.out.printf("filename : %s", opiniion.getFile());
+			opinion = new URL(homePage, "SUB/?ctId=15");
+			System.out.printf("protocal : %s", opinion.getProtocol());
+			System.out.printf("host : %s", opinion.getHost());
+			System.out.printf("port : %s", opinion.getPort());
+			System.out.printf("path : %s", opinion.getPath());
+			System.out.printf("filename : %s", opinion.getFile());
 			
+			BufferedReader br = new BufferedReader(new InputStreamReader(opinion.openStream()));
+			String line = null;
 			
+			while((line=br.readLine())!=null){
+				System.out.println(line);
+			}
+			br.close();
 		} catch (MalformedURLException e) {
 			System.out.println("잘못된 URL 주소");
 			e.printStackTrace();
